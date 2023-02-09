9ae36fe3d1d1f20cb3a91d33ceb21cce4565d86c
Dinolan <dinolann@runninghill.co.za>
2023-02-09 12:50:11 +0200

    Setting up login with JWT

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,40 +1,99 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const User = require('../models/user');
+const User = require("../models/user");
 
-router.post('/signup', (req,res,next) => {
+router.post("/signup", (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "Mail exists"
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user
+                            .save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: "User created"
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                });
+            }
+        });
+});
 
-    bcrypt.hash(req.body.password, 10, (err, has) =>{
-        if(err){
-            return res.status(500).json({
+router.post('/login',(req,res,next) => {
+    User.find({email: req.body.email})
+        .exec()
+        .then(user => {
+            if(user.length < 1){
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                    if(err){
+                        return res.status(401).json({
+                            message: 'Auth failed'
+                        });
+                    }
+                    if(result){
+                        return res.status(200).json({
+                        message: 'Auth succesful'
+                    })
+                    }
+
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
                 error: err
             });
-        } else{
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
+        });
+});
+
+router.delete("/:userId", (req, res, next) => {
+    User.remove({ _id: req.params.userId })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "User deleted"
             });
-            user
-                .save()
-                .then( result => {
-                    console.log(result);
-                    res.status(201).json({
-                        message: 'User created successfully'
-                    });
-                })
-                .catch(err =>{
-                    console.log(err);
-                    res.status(500).json({
-                        error: err
-                    });
-                })
-        }
-    });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
