d459849117f26580d85f3c42193e512f77274487
Dinolan <dinolann@runninghill.co.za>
2023-02-09 14:43:29 +0200

    PUT method added

--- api/routes/products.js
+++ api/routes/products.js
@@ -136,4 +136,30 @@ router.delete('/:productId', (req,res,next) => {
         });
 });
 
+router.put('/:productId', (req,res,next) => {
+    const id = req.params.productId;
+    const updateOps = {};
+
+    for(const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({_id: id }, {$set: updateOps })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'Car List Updated',
+                result
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+
+
 module.exports = router;
